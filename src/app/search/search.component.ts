71731d5bd948f8bf75e7f7c8fb4889335161a360
sathiraf <wsdfdo@gmail.com>
2020-02-01 10:48:07 +0530

    modified search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,18 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import {Location} from '@angular/common';
+
+import { FormControl } from '@angular/forms'; //auto complete
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  title = 'ng-autocompletesearch';///autoc
+  myControl = new FormControl();                //ac
+  options: string[] = ['One', 'Two', 'Three'];  //ac
+  filteredOptions: Observable<string[]>;
 
   constructor(private _location: Location) { }
 
+  
   ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
   }
+ 
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+ 
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
   btnBackUser= function () {
     this._location.back();
   };
 
+
+
+
 }
+
